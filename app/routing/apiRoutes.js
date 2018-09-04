03cb35c29a11fd985bbac234ca8efff3e594ad61
SchneiderMan <jschneid94@gmail.com>
2018-09-04 14:55:26 -0700

    Started script for apiRoutes, working on comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,6 +13,21 @@ module.exports = function(app) {
     // A POST routes /api/friends. 
     // This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function(req, res) {
+        var userInput = req.body;
+        console.log("Input = " + JSON.stringify(userInput));
+
+        var userResponses = userInput.scores;
+        console.log("Responses = " + userResponses);
+
+        for (var i = 0; i < friends.length; i++) {
+            // Compute differences for each question
+            var diff = 0;
+            for (var j = 0; j < userResponses.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+            }
+            console.log("diff = " + diff);
+        }
+
         friends.push(req.body);
         res.json(true);
     })
