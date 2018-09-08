5fa404761fbc2569e88096dbe137aeb4112f0db1
SchneiderMan <jschneid94@gmail.com>
2018-09-07 20:50:49 -0700

    Gave friends unique responses and leveled out their scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,47 +3,47 @@ var friendsArray = [
         "name":"Jonathan Van Ness",
         "photo":"https://cdn.graziadaily.co.uk/one/media/5b6a/cd34/31f6/c844/b59f/8a76/jonathan%20van%20ness.jpg?quality=50&width=960&ratio=16-9&resizeStyle=aspectfill&crop=263,0,1782,2700&resize=crop&format=jpg",
         "scores":[
+            5,
+            3,
             1,
             1,
+            2,
+            2,
+            2,
             1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
+            4,
+            5
         ]
     },
     {
         "name":"Antoni Porowski",
         "photo":"https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/03/Antoni_1-920x584.jpg",
         "scores":[
-            5,
-            1,
             4,
             4,
-            5,
-            1,
+            3,
             2,
             5,
             4,
-            1
+            4,
+            3,
+            4,
+            3
         ]
     },
     {
         "name":"Bobby Berk",
         "photo":"https://biowikis.com/wp-content/uploads/2018/02/Bobby-Berk-Bio-Wiki-Net-Worth.jpg",
         "scores":[
+            3,
             5,
-            1,
-            4,
-            4,
             5,
-            1,
+            4,
             2,
-            5,
+            3,
+            3,
             4,
+            3,
             1
         ]
     },
@@ -51,32 +51,32 @@ var friendsArray = [
         "name":"Tan France",
         "photo":"https://www.etonline.com/sites/default/files/styles/max_970x546/public/images/2018-02/tan_france.jpg?itok=fRLTRZKJ&h=33978f1b",
         "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
+            2,
+            3,
             5,
+            3,
+            2,
             4,
-            1
+            2,
+            4,
+            2
         ]
     },
     {
         "name":"Karamo Brown",
         "photo":"http://www.goliathatlanta.com/wp-content/uploads/2018/03/Karamo-Brown-10.jpg",
         "scores":[
-            5,
-            1,
-            4,
             4,
             5,
-            1,
+            4,
+            3,
+            4,
             2,
+            4,
             5,
             4,
-            1
+            3
         ]
     }
 ];
