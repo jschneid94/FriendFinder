2ec4cb4f302fc100893946b16bcc9642a7e7d63e
SchneiderMan <jschneid94@gmail.com>
2018-09-07 20:28:05 -0700

    Removed local images and added all friends objects

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
         "name":"Jonathan Van Ness",
-        "photo":"../public/images/jvn.jpg",
+        "photo":"https://cdn.graziadaily.co.uk/one/media/5b6a/cd34/31f6/c844/b59f/8a76/jonathan%20van%20ness.jpg?quality=50&width=960&ratio=16-9&resizeStyle=aspectfill&crop=263,0,1782,2700&resize=crop&format=jpg",
         "scores":[
             1,
             1,
@@ -16,8 +16,56 @@ var friendsArray = [
         ]
     },
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Antoni Porowski",
+        "photo":"https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/03/Antoni_1-920x584.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Bobby Berk",
+        "photo":"https://biowikis.com/wp-content/uploads/2018/02/Bobby-Berk-Bio-Wiki-Net-Worth.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Tan France",
+        "photo":"https://www.etonline.com/sites/default/files/styles/max_970x546/public/images/2018-02/tan_france.jpg?itok=fRLTRZKJ&h=33978f1b",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Karamo Brown",
+        "photo":"http://www.goliathatlanta.com/wp-content/uploads/2018/03/Karamo-Brown-10.jpg",
         "scores":[
             5,
             1,
